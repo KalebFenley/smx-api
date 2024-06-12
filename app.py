af28a6574fed61cc306be1f718fd4bfc64016daa
Kaleb Fenley <kalebfenley@gmail.com>
2024-06-12 17:22:33 -0500

    added another function to pull device list from smx

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import os
 import requests
 from dotenv import load_dotenv
 
+'''
+Before using this please ensure the following environment variables are set, or built in .env file:
+
+SMX_API_URL: The base URL of the SMX API (e.g., "https://{smx_ip}:18443/rest/v1/").
+SMX_USERNAME: The username for authentication with the SMX API.
+SMX_PASSWORD: The password for authentication with the SMX API.
+'''
+
+
 # load from .env file
 load_dotenv
 
@@ -13,20 +22,28 @@ headers = {
     'Content-Type': 'application/json'
 }
 
+def query_devices():
+    endpoint = "config/device"
+    params = {
+        "offset":0,
+        "limit":0,
+    }
+    r = requests.get(f'{api_url}{endpoint}', params=params, auth=(username, password), headers=headers, verify=False)
+    try:
+        r.raise_for_status()
+        data = r.json()
+    except requests.exceptions.HTTPError as err:
+        print(f'HTTP error occurred: {err}')
+        return(False)
+    except requests.exceptions.RequestException as err:
+        print(f'Request exception occurred: {err}')
+        return(False)
+    return(data)
+
 def query_alarms():
     """
-    Queries the SMX API for a list of standing alarms.
-
-    This function retrieves a list of standing alarms from the SMX API. 
-    **Before using this function, please ensure the following environment variables are set, or built in .env file:**
-
-    * `SMX_API_URL`: The base URL of the SMX API (e.g., "https://{smx_ip}:18443/rest/v1/").
-    * `SMX_USERNAME`: The username for authentication with the SMX API.
-    * `SMX_PASSWORD`: The password for authentication with the SMX API.
-
-    **Security Note:** It's highly recommended to store sensitive information like credentials in environment variables 
-    instead of directly in your code. This improves security and makes code management easier.
-
+    This function retrieves a list of standing alarms from the SMX API.
+    
     Returns:
         dict: A dictionary containing the response data from the SMX API, including a list of standing alarms on success.
 
@@ -50,8 +67,8 @@ def query_alarms():
         print(f'Request exception occurred: {err}')
     return(data)
 
+# Pull all standing alarms from SMX
 alarm_list = query_alarms()
-
 """
 Basic example of parsing alarms.
 Ideally you would have more logic in place to alert/discard based on alarm type/severity...ect
@@ -61,3 +78,10 @@ for alarm in alarm_list:
         # Here you would call a function to alert.
         for i in alarm:
             print(f"{i:30} {alarm[i]}")
+
+# Pull Device Information from SMX
+device_list = query_devices()
+# Build a dict contianing hostname:ipaddress for every Calix OLT
+hostname_ip_dict = {}
+for device in device_list:
+    hostname_ip_dict[f"{device['hostname']}"] = device["address"]
